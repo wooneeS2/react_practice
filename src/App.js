3275952209a63893eea7266171184abdd9078599
woonees2 <soyoung0007@naver.com>
2020-10-11 02:50:30 +0900

    오늘은 여기까지

--- src/App.js
+++ src/App.js
@@ -1,57 +1,69 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
-//App클래스가 react.component클래스를 상속받도록 extends React.Component를 붙이는 것
-//상속 -> 클래스에 다른 클래스의 기능을 추가할 수 있게 해주는 것
 class App extends React.Component {
-
-  constructor(props){
-    super(props);
-    console.log('hello');
-  }
-
-
-  state = {   //객체 형태의 데이터
-    count : 0,
-
+  state ={ 
+    isLodading: true,
+    movies: [],
   };
 
-  add = () => {
-    this.setState(current => ({count: current.count+1,}));
+  getMovies = async ()=> {
+    const {
+      data : {
+        data: {movies},
+      },
+    }=  await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");  
+    this.setState({movies,isLoading: false});
+    //es6에서는 객체의 키와 대입할 변수의 이름이 같다면 코드를 축약할 수 있음.
   };
+  
 
-  minus = () => {
-    this.setState(current => ({count: current.count -1,}));
 
-    };
-
-    componentDidMount(){
-      console.log('Compenent rendered');
-    }
-
-    componentDidUpdate(){
-      console.log("I just updated");
-    }
-
-    componentWillUnmount(){
-      console.log("bye-ru");
-    }
+componentDidMount(){
+  //영화 데이터 로딩
+  this.getMovies();
+}
 
   render(){
-    console.log("I'm rendering");
-    console.log('render');
-    return(
-      <div>
-     <h1>the number is: { this.state.count}</h1>
-    {/* jsx를 반환하기 위하여 render() 사용 */}
-    <button  onClick={this.add}> Add</button>
-    <button onClick = {this.minus}>  Minus</button>
+    const { isLoading, movies } = this.state;
+    return (
+    <section className="container">
+       { isLoading ? (
+      <div className="loader">
+      <span className = "loader__text">Loding...</span>
+      </div>
+    )
+      
+      :(
+        <div className="movies">
+        { movies.map((movie)=>(
+      
+      
+        <Movie
+        key={movie.id }
+        id={movie.id}
+        year={movie.year}
+        title={movie.title}
+        summary={movie.summary}
+        poster={movie.medium_cover_image}
+        genres={movie.genres}
+        />
+        
+      ))}
+     
 
-    </div>
+      
+    
+      </div>
+      )}
+    </section>
+
     );
   }
 
-} 
-
+}
 
 
 export default App;
