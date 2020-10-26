79c7d9a432c6636cf17c22336c33702d0840fe3b
woonees2 <soyoung0007@naver.com>
2020-10-26 20:42:59 +0900

    Updates

---
+++ precache-manifest.09a6e3291cfb6a5a153e06c23d9310b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35619a008ee849921afc38829acfd1e",
+    "url": "/react_practice/index.html"
+  },
+  {
+    "revision": "db3f7c42dbc9d61b84fe",
+    "url": "/react_practice/static/css/main.8c4dca47.chunk.css"
+  },
+  {
+    "revision": "7017f2d9cb1c61aedaa2",
+    "url": "/react_practice/static/js/2.9a71132e.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/react_practice/static/js/2.9a71132e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db3f7c42dbc9d61b84fe",
+    "url": "/react_practice/static/js/main.94042986.chunk.js"
+  },
+  {
+    "revision": "c84bbd9d2e55c20f2cd8",
+    "url": "/react_practice/static/js/runtime-main.f6068f60.js"
+  }
+]);
