4d5d5c773181a5184208d81460bbada005e5f293
woonees2 <soyoung0007@naver.com>
2020-10-26 20:33:31 +0900

    일단완성

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Navigation.css';
 
 function Navigation() {
     return (
         <div className="nav">
-            <Link to ="/">Home</Link>
-            <Link to ="/abot">About</Link>
+            <Link to="/">Home</Link>
+            <Link to="/abot">About</Link>
             {/* <a>는 페이지를 새로고침하므로 라우터의 Link to 태그를 이용함. */}
         </div>
+
     );
 }
 
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,20 +1,36 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 
 
-class Detail extends React.Component{
-    componentDidMount(){
-        const{ location, history} = this.props;
+class Detail extends React.Component {
+    componentDidMount() {
+        const { location, history } = this.props;
+        if (location.state == undefined) {
+            history.push('/');      //location.state가 없는 경우 home으로 이동
+
+            //render -> componentDidMount의 순서대로 함수를 실행하기때문에
+            // render내에서 location.state.title이 undefined이기 때문에 리다이렉트 작동불가
+        }
     }
 
-    render(){
-        return <span>hello</span>;
+
+    render() {
+        const { location } = this.props;
+        if (location.state) {
+            return (
+                <span>{location.state.title}</span>
+
+            );
+        }
+        else {
+            return null;
+            //location.state가 없으면 null 반환
+        }
+
     }
 }
 
 
-function Detail(props){
-    console.log(props);
-    return <span>hello</span>
-}
+
 
 export default Detail;
